dea3009c3f512002fb6f5a14c1c5a7e3bcdc09a5
Faiq Khan <shayankhan480@gmail.com>
2022-10-07 20:05:42 +0500

    integrate uploas api..

---
+++ lib/models/comment.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'comment.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Comment _$CommentFromJson(Map<String, dynamic> json) => Comment(
+      userId: json['UserID'] as int?,
+      fullName: json['FullName'] as String?,
+      userName: json['UserName'] as String?,
+      profilePic: json['ProfilePic'] as String?,
+      feedId: json['FeedID'] as int?,
+      postViews: json['PostViews'] as int?,
+      postLikes: json['PostLikes'] as int?,
+      postShares: json['PostShares'] as int?,
+      postComments: json['PostComments'] as int?,
+      feedDate: json['FeedDate'] == null
+          ? null
+          : DateTime.parse(json['FeedDate'] as String),
+      comment: json['Comment'] as String?,
+    );
